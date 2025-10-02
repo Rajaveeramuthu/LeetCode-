03436a34dc5f8a921b499276667b1f69d1735cd0
_RVM_ <rajaveeramuthu2509@gmail.com>
2025-10-02 12:32:03 +0530

    Time: 0 ms (100%), Space: 40.8 MB (73.55%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.java
+++ 3100-water-bottles-ii/3100-water-bottles-ii.java
@@ -3,10 +3,10 @@ class Solution {
         int drunk = numBottles;   
         int empty = numBottles;  
         while (empty >= numExchange) {
-            empty -= numExchange;  // exchange-க்கு கொடுக்கிறோம்
-            drunk++;               // அந்த full bottle குடித்தோம்
-            empty++;               // குடித்த bottle -> empty
-            numExchange++;         // exchange requirement +1
+            empty -= numExchange; 
+            drunk++;               
+            empty++;               
+            numExchange++;         
         }
 
         return drunk;
