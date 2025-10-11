d81da75773cdfb3edd206511ff86601675771dac
_RVM_ <rajaveeramuthu2509@gmail.com>
2025-10-11 20:59:38 +0530

    Time: 223 ms (65.76%), Space: 61.9 MB (72.97%) - LeetHub

---
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public long maximumTotalDamage(int[] power) {
+        Map<Integer, Long> map = new HashMap<>();
+        for (int p : power) {
+            map.put(p, map.getOrDefault(p, 0L) + p);
+        }
+
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+
+        int n = keys.size();
+        long[] dp = new long[n];
+        dp[0] = map.get(keys.get(0));
+
+        for (int i = 1; i < n; i++) {
+            long curr = map.get(keys.get(i));
+            int j = i - 1;
+            while (j >= 0 && keys.get(i) - keys.get(j) <= 2) j--;
+            long include = curr + (j >= 0 ? dp[j] : 0);
+            long exclude = dp[i - 1];
+            dp[i] = Math.max(include, exclude);
+        }
+
+        return dp[n - 1];
+    }
+}
